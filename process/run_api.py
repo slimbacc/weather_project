1dce265b9b3ea9a1730fcc99f6a5cb04d279d3a8
slimbacc <142884305+slimbacc@users.noreply.github.com>
2023-10-03 14:32:22 +0100

    Update run_api.py

--- process/run_api.py
+++ process/run_api.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pyrebase
 from swagger_client.rest import ApiException
 
-from weather_pipeline.config.config import app_config
+from weather_pipeline.weather_project.config.config import app_config
 from weather_pipeline.process.functions import flatten_dict
 
 api_instance, q, firebaseConfig= (
